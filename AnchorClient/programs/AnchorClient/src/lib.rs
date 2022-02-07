f3d32078f7c7e04d78cd43a7cbd6a8f439b2d210
polea003 <polea003@fiu.edu>
2022-02-06 23:41:18 -0500

    increment vote feature

--- AnchorClient/programs/AnchorClient/src/lib.rs
+++ AnchorClient/programs/AnchorClient/src/lib.rs
@@ -6,9 +6,38 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod anchor_client {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs.
+        base_account.total_votes = 0;
+        Ok(())
+    }
+
+    pub fn add_vote(ctx: Context<AddVote>) -> ProgramResult {
+        // Get a reference to the account and increment total_gifs.
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_votes += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddVote<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_votes: u64,
+} 
--- AnchorClient/tests/AnchorClient.js
+++ AnchorClient/tests/AnchorClient.js
@@ -1,12 +1,44 @@
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
+
 
 const main = async() => {
   console.log("🚀 Starting test...");
-  anchor.setProvider(anchor.Provider.env());
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.AnchorClient;
-  const tx = await program.rpc.initialize();
+
+
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Vote Count', account.totalVotes.toString())
+
+  // Call add_vote!
+  await program.rpc.addVote({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+
+  // Get the account again to see what changed.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Vote Count', account.totalVotes.toString())
 }
 
 const runMain = async () => {
