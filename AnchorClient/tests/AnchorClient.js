7dbb1c43b71b5558202415dd63d32ef3f1cc5f3c
polea003 <polea003@fiu.edu>
2022-02-06 23:27:13 -0500

    custom test script

--- AnchorClient/tests/AnchorClient.js
+++ AnchorClient/tests/AnchorClient.js
@@ -1,14 +1,22 @@
 const anchor = require('@project-serum/anchor');
 
-describe('AnchorClient', () => {
-
-  // Configure the client to use the local cluster.
+const main = async() => {
+  console.log("🚀 Starting test...");
   anchor.setProvider(anchor.Provider.env());
+  const program = anchor.workspace.AnchorClient;
+  const tx = await program.rpc.initialize();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.AnchorClient;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
-});
+  console.log("📝 Your transaction signature", tx);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain(); 
